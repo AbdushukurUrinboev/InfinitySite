010cfa7dbc3b2713b0956afc91b0146b632e4d59
Bakhromjon Kosimov <kosimovbakhromjon@gmail.com>
2022-02-07 14:14:14 +0500

    please get fontawesome link to index.html

--- src/components/Gallereya/Gallereya.jsx
+++ src/components/Gallereya/Gallereya.jsx
@@ -3,7 +3,37 @@ import './Gallereya.css';
 
 function Gallereya () {
     return(
-        <h1>Hello Gallereya</h1>
+      
+        <div className="container">
+  <p className='toGallerySize'>Bizning Gallery</p>
+  <div className="row row-cols-2 row-cols-lg-3">
+    <div className="col-12 col-lg-8 " >
+        <div className='firstPicture'>
+             <p className='cardText'>Welcome to our outstanding Infinity center. This is your place, feel here as your home sit, amet consectetur adipisicing elit!</p>
+        </div>
+      </div>
+    <div className="col-6 col-lg-4 ">
+        <div className='secondCard'>
+
+        </div>
+    </div>
+    <div className="col-6 col-lg-4">
+        <div className="thirdCard">
+
+        </div>
+    </div>
+    <div className="col-6 col-lg-4 ">
+        <div className="fourthCard">
+
+        </div>
+    </div>
+    <div className="col-6 col-lg-4">
+        <div className="fifthCard">
+
+        </div>
+    </div>
+  </div>
+</div>
     )
 }
 
