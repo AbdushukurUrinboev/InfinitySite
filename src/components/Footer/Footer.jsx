feef78a78e0d765c2ef19e3cce3e67c71b41fba8
Bakhromjon Kosimov <kosimovbakhromjon@gmail.com>
2022-02-22 23:24:19 +0500

    O'zgartirishlar yakunlandi faqat mobil versiyasida muommolar bor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import './Footer.css';
-import grouplogo from '../../assets/logotip.png'
+import grouplogo from '../../assets/logo.svg'
+import pin from '../../assets/pin.svg'
+import facebook from '../../assets/facebook.svg'
+import instagram from '../../assets/instagram.svg'
+import twitter from '../../assets/twitter.svg'
+import telegram from '../../assets/telegram.svg'
 
 
 function Footer() {
@@ -14,7 +19,7 @@ function Footer() {
                     <div className="row row-cols-2 ">
                         <div className="col-12  col-lg-12 ">
                             <div className="row rowContainer">
-                                <div className="col-4 col-lg-2">
+                                <div className="col-4 col-lg-3">
                                     <p className='headLine'>DASTURLASH</p>
                                     <p className='smallTexts'>Frontend</p>
                                     <p className='smallTexts'>Backend</p>
@@ -22,7 +27,7 @@ function Footer() {
                                     <p className='smallTexts'>UI and UX designer</p>
                                     <p className='smallTexts'>Web designer</p>
                                 </div>
-                                <div className="col-4 col-lg-2">
+                                <div className="col-4 col-lg-3">
                                     <p className='headLine'>ENGLISH</p>
                                     <p className='smallTexts'>English for kids</p>
                                     <p className='smallTexts'>General English</p>
@@ -30,7 +35,7 @@ function Footer() {
                                     <p className='smallTexts'>IELTS</p>
 
                                 </div>
-                                <div className="col-4 col-lg-2">
+                                <div className="col-4 col-lg-3">
                                     <p className='headLine'>MATEMATIKA</p>
                                     <p className='smallTexts'>Mental arifmetika</p>
                                     <p className='smallTexts'>Maths for kids</p>
@@ -38,25 +43,23 @@ function Footer() {
 
 
                                 </div>
-                                <div className="col-12 col-lg-3">
+                                <div className="col-12 col-lg-1">
 
 
                                 </div>
-                                <div className="col-12 col-lg-3">
-                                    <div className="row">
-                                        <div className="col-4 footerLine">
+                                <div className="col-12 col-lg-2">
+                                   
+                                        <div className="col-12">
                                             <img className='logoImage' src={grouplogo} alt="Bu joyda rasm bor!" />
                                         </div>
-                                        <div className="col-4">
-                                            <p className='footerInfinity'>Infinity</p>
-                                        </div>
-                                    </div>
-                                    <i className="fas fa-map-marker-alt firstIcon"></i>
-                                    <p>Farg'ona, Uzbekistan</p>
-                                    <a href="#"><i className="fab fa-facebook-f firstInlinedIcons "></i></a>
-                                    <a href="#"><i className="fab fa-instagram inlinedIcons"></i></a>
-                                    <a href="#"><i className="fab fa-twitter inlinedIcons"></i></a>
-                                    <a href="#"><i className="fab fa-telegram-plane inlinedIcons"></i></a>
+                                       
+                                    
+                                    <i className="fas fa-map-marker-alt "></i>
+                                     <a href="#"><img src={pin} className="footerIcons" alt="Buyerda rasim bor"  /></a><p>Farg'ona, Uzbekistan</p>
+                                    <a href="#"><img src={facebook} className="footerIcons" alt="Buyerda rasim bor"  /></a>
+                                    <a href="#"><img src={instagram} className="footerIcons" alt="Buyerda rasim bor" /></a>
+                                    <a href="#"><img src={twitter} className="footerIcons" alt="Buyerda rasim bor" /></a>
+                                    <a href="#"><img src={telegram} className="footerIcons" alt="Buyerda rasim bor" /></a>
 
                                 </div>
 
@@ -66,12 +69,14 @@ function Footer() {
                         </div>
 
                     </div>
-                    <hr />
-
-                    <span className='footerWords'>2022. All Rights reserved</span>
-                    <span className='footerNumber'>+9989 633 55 99</span>
 
                 </div>
+                   <hr />
+                    <div className="container">
+                    <span className='footerWords'>2022. All Rights reserved</span>
+                    <span className='footerNumber'>+9989 633 55 99</span>
+                    </div>
+                    
             </footer>
         </div>
 
