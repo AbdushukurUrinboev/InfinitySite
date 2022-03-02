380de711a5fb674acb4add0a010f607df2596bfd
Adulboriy Nabijonov <abdulboriiyynabijonov@gmail.com>
2022-03-02 18:37:29 +0500

    All pages updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css'
 import Header from "./components/Header/Header";
-import AsosiyYunalishlar from "./components/AsosiyYunalish/AsosiyYunalishlar.jsx";
+import AsosiyYunalishlar from "./components/AsosiyYunalish/AsosiyYunalishlar";
 import NegaAynanBiz from "./components/NegaAynanBiz/NegaAynanBiz";
 import Gallereya from "./components/Gallereya/Gallereya";
 import Footer from "./components/Footer/Footer";
