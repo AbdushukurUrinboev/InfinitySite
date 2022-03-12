a80c38bd4d0587b74e6a79ef76ecdd913ddc1e42
Adulboriy Nabijonov <abdulboriiyynabijonov@gmail.com>
2022-03-12 23:29:50 +0500

    Media version upfatef

--- src/components/Gallereya/Gallereya.jsx
+++ src/components/Gallereya/Gallereya.jsx
@@ -18,47 +18,47 @@ function Gallereya() {
 
     <div className="ourGallery">
         <div className="container">
-        <p className='toGallerySize'>Bizning Gallery</p>
-        <div className="row row-cols-2 row-cols-lg-3 rowGalleryCards">
+          <p className='toGallerySize'>Bizning Gallery</p>
+          <div className="row row-cols-2 row-cols-lg-3 rowGalleryCards">
 
-          <div data-aos="fade-down" className="col-12 col-lg-8 exraPadding cardGallery cardGallery1" >
-              <img className='firstPicture' src={firstPic} alt="Bu joyda rasm bor!" />
-              <div class="infoGalleryFirst">
-                <p className="infoGalleryFirstText">Bizda siz yangi kasbni o'rganishingiz yoki yangi ko'nikmalarga ega bo'lishingiz mumkin. 
-                Bizning dasturlarimiz amaliyotga yo'naltirilgan, biz materialning dolzarbligini 
-                kuzatib boramiz va ish va amaliyotda yordam beramiz.</p>
+            <div data-aos="fade-down" className="col-12 col-lg-8 exraPadding cardGallery cardGallery1" >
+                <img className='firstPicture' src={firstPic} alt="Bu joyda rasm bor!" />
+                <div class="infoGalleryFirst">
+                  <p className="infoGalleryFirstText">Bizda siz yangi kasbni o'rganishingiz yoki yangi ko'nikmalarga ega bo'lishingiz mumkin. 
+                  Bizning dasturlarimiz amaliyotga yo'naltirilgan, biz materialning dolzarbligini 
+                  kuzatib boramiz va ish va amaliyotda yordam beramiz.</p>
+              </div>
             </div>
-          </div>
 
-          <div data-aos="fade-down" className="col-6 col-lg-4 exraPadding cardGallery">
-            <img className='secondPic' src={secondPic} alt="Bu joyda rasm bor!" />
-            <div class="infoGallery">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit sit amet consectetur </p>
+            <div data-aos="fade-down" className="col-6 col-lg-4 exraPadding cardGallery">
+              <img className='secondPic' src={secondPic} alt="Bu joyda rasm bor!" />
+              <div class="infoGallery">
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit sit amet consectetur </p>
+              </div>
             </div>
-          </div>
 
-          <div data-aos="fade-up" className="col-6 col-lg-4 exraPadding cardGallery">
-            <img className='thirdPic ' src={thirdPic} alt="Bu joyda rasm bor!" />
-            <div class="infoGallery">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit sit amet consectetur </p>
+            <div data-aos="fade-up" className="col-6 col-lg-4 exraPadding cardGallery">
+              <img className='thirdPic ' src={thirdPic} alt="Bu joyda rasm bor!" />
+              <div class="infoGallery">
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit sit amet consectetur </p>
+              </div>
             </div>
-          </div>
 
-          <div data-aos="fade-up" className="col-6 col-lg-4 exraPadding cardGallery">
-            <img className='fourthPic ' src={fourthPic} alt="Bu joyda rasm bor!" />
-            <div class="infoGallery">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit sit amet consectetur </p>
+            <div data-aos="fade-up" className="col-6 col-lg-4 exraPadding cardGallery">
+              <img className='fourthPic ' src={fourthPic} alt="Bu joyda rasm bor!" />
+              <div class="infoGallery">
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit sit amet consectetur </p>
+              </div>
             </div>
-          </div>
 
-          <div data-aos="fade-up" className="col-6 col-lg-4 exraPadding cardGallery">
-            <img className='fifthPic ' src={fifthPic} alt="Bu joyda rasm bor!" />
-            <div class="infoGallery">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit sit amet consectetur </p>
+            <div data-aos="fade-up" className="col-6 col-lg-4 exraPadding cardGallery">
+              <img className='fifthPic ' src={fifthPic} alt="Bu joyda rasm bor!" />
+              <div class="infoGallery">
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit sit amet consectetur </p>
+              </div>
             </div>
-          </div>
 
-        </div>
+          </div>
       </div>
     </div>
   )
